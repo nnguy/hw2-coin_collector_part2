b831fb0d452f9c87fb6ec2b49db269aeced323e4
nnguy <nn1248@nyu.edu>
2022-09-29 16:22:20 -0400

    create new main2.cpp for the game

---
+++ main2.cpp
@@ -0,0 +1,25 @@
+/*
+Nelson Nguy 
+CSC 211H
+Assignment 2.5 - This program utilizes the Point, World, and Robot classes to 
+
+*/
+#include <iostream>
+#include <stdlib.h>
+#include <cctype>
+#include "point.h"
+#include "world.h"
+#include "robot.h"
+
+using namespace std; 
+
+int main(int argc, char* argv[]){
+    //create world, robot
+
+    //check for command line input for the coordinates 
+
+    //create a function in world that prints the world  
+    //create a funciton in robot that moves via random directions
+    //create a function in robot that receives arrow key input (or wasd)
+    //clear the screen after every movement 
+}
