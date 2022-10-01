5b28e1f3dd8ab801e473ed860f903a4167fa9f88
nnguy <nn1248@nyu.edu>
2022-10-01 01:47:53 -0400

    removed extra text at the end

--- main2.cpp
+++ main2.cpp
@@ -182,15 +182,4 @@ int main(int argc, char* argv[]){
     return 0; 
     
     
-
-    //check for command line input for the coordinates 
-    //case of argc = 1, that means no command line arguments wre included. So have the user input 
-    //case of argc = 7, that means that the user attempted to put in coordinates via command line 
-    //case of argc >1 and != 7, throw an error message 
-    
-    //create a function in world that prints the world  
-    //create a funciton in robot that moves via random directions
-    //create a function in robot that receives arrow key input (or wasd)
-    //clear the screen after every movement 
-
 }
