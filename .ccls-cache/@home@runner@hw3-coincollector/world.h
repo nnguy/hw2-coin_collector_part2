07e09ef666050622a71e7885509a7fcd6498b3b5
nnguy <36558416+nnguy@users.noreply.github.com>
2022-10-11 23:32:33 +0000

    overloaded ++ and -- operators

---
+++ .ccls-cache/@home@runner@hw3-coincollector/world.h
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+#ifndef WORLD_H
+#define WORLD_H
+
+#include "point.h"
+const int MAX_POINTS = 3; 
+
+class World{
+private:
+  Point* points[MAX_POINTS]; 
+public:
+  World(); 
+  ~World(); 
+  void print() const; 
+  void set(int, int, int); //parameter 1 is the index, parameter 2 is the x coordinate, parameter 3 is the y coordinate 
+  Point getPoint(int); 
+  void releasePoint(int); 
+  bool isPointExistent(int); 
+  bool isPointExistent(int, int); 
+};
+
+
+#endif
